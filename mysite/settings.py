3ddcf08e130ca7d902809f5b03013f7b8af13ba0
Friday <malatefriday12@gmail.com>
2022-02-21 17:53:39 +0200

    updates

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-se7bxj4^ef(u0b^&yk%)6@_3nx4u70w91!4t00gyym+&km9)--'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['malate.herokuapp.com']
@@ -129,9 +129,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-
+'''
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+'''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
